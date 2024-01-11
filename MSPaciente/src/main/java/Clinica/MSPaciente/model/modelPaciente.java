3fc6b3d08dcb1591369d5a0a9e085a032a5c9890
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-10 22:49:54 -0500

    CREATE ModelPaciente

--- MSPaciente/src/main/java/Clinica/MSPaciente/model/modelPaciente.java
+++ MSPaciente/src/main/java/Clinica/MSPaciente/model/modelPaciente.java
@@ -1,8 +1,71 @@
 package Clinica.MSPaciente.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 @Entity
 @Table(name="paciente")
 public class modelPaciente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pacienteid")
+    public int pacienteid;
+
+    @Column(name = "nombre")
+    public String nombre;
+
+    @Column(name = "apellido")
+    public String apellido;
+
+    @Column(name = "genero")
+    public String genero;
+
+    @Column(name = "celular")
+    public String celular;
+
+    public int getPacienteid() {
+        return pacienteid;
+    }
+
+    public void setPacienteid(int pacienteid) {
+        this.pacienteid = pacienteid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
     
 }
