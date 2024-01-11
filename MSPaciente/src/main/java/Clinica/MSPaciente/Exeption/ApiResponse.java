712440a52b0320bde8a9162854615d3e49deb4f6
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-10 22:51:19 -0500

    CREATE EXCEPTION

---
+++ MSPaciente/src/main/java/Clinica/MSPaciente/Exeption/ApiResponse.java
@@ -0,0 +1,28 @@
+package Clinica.MSPaciente.Exeption;
+
+public class ApiResponse<T> {
+    
+    private final int status;
+    private final String mensaje;
+    private final T data;
+    
+    public ApiResponse(int status, String mensaje, T data) {
+        this.status = status;
+        this.mensaje = mensaje;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    
+}
