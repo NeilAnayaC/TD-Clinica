edf64db7c279301173e22283f4468065db6668d3
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-10 21:03:21 -0500

    CREATE Archivo Spring Initializr

---
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/MsHisctoriaCApplication.java
@@ -0,0 +1,13 @@
+package Clinica.MSHisctoriaC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHisctoriaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHisctoriaCApplication.class, args);
+	}
+
+}
