a0d2f6fad4f9bf465fcfe48a11a233a0e0c4d16a
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-18 23:45:07 -0500

    Create Controller detalle and historiaPostgre

---
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/Controller/DetalleController.java
@@ -0,0 +1,109 @@
+package Clinica.MSHisctoriaC.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import Clinica.MSHisctoriaC.Exection.ApiResponseH;
+import Clinica.MSHisctoriaC.Exection.HistoriaNotFoundException;
+import Clinica.MSHisctoriaC.Mapper.DetalleMapper;
+import Clinica.MSHisctoriaC.dto.DetalleRequest;
+import Clinica.MSHisctoriaC.model.modelDetalle;
+import Clinica.MSHisctoriaC.service.IDetalleService;
+
+@RestController
+@RequestMapping("/api/detalle")
+public class DetalleController {
+    
+    @Autowired
+    IDetalleService detalleService;
+
+    @Autowired
+    DetalleMapper detalleMapper;
+
+    
+    @GetMapping("/getAll/detalle")
+        public ResponseEntity<ApiResponseH<List<DetalleRequest>>> getAll(){
+            try{
+                List<modelDetalle> detalles = detalleService.findAll();
+                List<DetalleRequest> detalleRequests = detalles.stream()
+                .map(detalleMapper::entityToDto)
+                .collect(Collectors.toList());
+                return ResponseEntity.ok(new ApiResponseH<>(HttpStatus.OK.value(),
+                 "Consulta de listado exitosa", detalleRequests));
+            }catch(Exception ex){
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                 "Error al procesar la solicitud", null));
+            }
+        }
+
+        //busqueda por ID
+    @GetMapping("/getById/detalle")
+    public ResponseEntity<ApiResponseH<DetalleRequest>> getById(@RequestParam int id){
+        //sfinal String mensaje1 = "eroor al buscar paciente";
+        try{
+            
+            modelDetalle modelDetalle = detalleService.findById(id);
+            if(modelDetalle !=null){
+                DetalleRequest detalleRequest = detalleMapper.entityToDto(modelDetalle);
+                return ResponseEntity.ok(new ApiResponseH<>(HttpStatus.OK.value(),
+                 "Consulta de Busqueda por ID exitosa", detalleRequest));
+            }else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ApiResponseH<>(HttpStatus.NOT_FOUND.value(),
+                 "Paciente no encontrado", null));
+            }
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "eroor al buscar paciente", null));
+        }
+    }
+
+    //crear paciente
+    @PostMapping("/create/detalle")
+    public ResponseEntity<ApiResponseH<DetalleRequest>> create(@RequestBody DetalleRequest detalleRequest) {
+        try{
+            modelDetalle detalleModel = detalleMapper.dtoTOEntity(detalleRequest);
+            detalleModel = detalleService.add(detalleModel);
+            DetalleRequest creaDetalleRequest = detalleMapper.entityToDto(detalleModel);
+            return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponseH<>(HttpStatus.CREATED.value(), 
+            "Paciente creado", creaDetalleRequest));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+            "Error al crear paciente", null));
+        }
+    }
+
+    //Modificar paciente
+    @PutMapping("/update/detalle")
+    public ResponseEntity<ApiResponseH<DetalleRequest>> update(@RequestBody DetalleRequest detalleRequest) {
+        try{
+            modelDetalle detalle = detalleService.update(detalleMapper.dtoTOEntity(detalleRequest));
+            DetalleRequest actualizarDetalleRequest = detalleMapper.entityToDto(detalle);
+            return ResponseEntity.ok(new ApiResponseH<>(HttpStatus.OK.value(),
+             "Paciente actualizado", actualizarDetalleRequest));
+        }catch(HistoriaNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ApiResponseH<>(HttpStatus.NOT_FOUND.value(), 
+            ex.getMessage(), null));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "Error al actualizar paciente", null));
+        }
+    }
+}
---
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/Controller/HistoriaController.java
@@ -0,0 +1,108 @@
+package Clinica.MSHisctoriaC.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import Clinica.MSHisctoriaC.Exection.ApiResponseH;
+import Clinica.MSHisctoriaC.Exection.HistoriaNotFoundException;
+import Clinica.MSHisctoriaC.Mapper.HistoriaMapper;
+import Clinica.MSHisctoriaC.dto.HistoriaRequest;
+import Clinica.MSHisctoriaC.model.modelHistoria;
+import Clinica.MSHisctoriaC.service.IHistoriaService;
+
+@RestController
+@RequestMapping("/api/detalle")
+public class HistoriaController {
+    
+    @Autowired
+    IHistoriaService historiaService;
+
+    @Autowired
+    HistoriaMapper historiaMapper;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<ApiResponseH<List<HistoriaRequest>>> getAll(){
+        try{
+            List<modelHistoria> historias = historiaService.findAll();
+            List<HistoriaRequest> historiaRequests = historias.stream()
+            .map(historiaMapper::entityToDto)
+            .collect(Collectors.toList());
+            return ResponseEntity.ok(new ApiResponseH<>(HttpStatus.OK.value(),
+            "Consulta de listado de historia Exitosa", historiaRequests));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                 "Error al procesar la solicitud", null));
+        }
+    }
+
+     @GetMapping("getById")
+    public ResponseEntity<ApiResponseH<HistoriaRequest>> getById(@RequestParam int id){
+        //sfinal String mensaje1 = "eroor al buscar paciente";
+        try{
+            
+            modelHistoria modelHistoria = historiaService.findById(id);
+            if(modelHistoria !=null){
+                HistoriaRequest historiaRequest = historiaMapper.entityToDto(modelHistoria);
+                return ResponseEntity.ok(new ApiResponseH<>(HttpStatus.OK.value(),
+                 "Consulta de Busqueda por ID exitosa", historiaRequest));
+            }else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ApiResponseH<>(HttpStatus.NOT_FOUND.value(),
+                 "Paciente no encontrado", null));
+            }
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "eroor al buscar paciente", null));
+        }
+    }
+
+    //crear paciente
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponseH<HistoriaRequest>> create(@RequestBody HistoriaRequest historiaRequest) {
+        try{
+            modelHistoria modelHistoria = historiaMapper.dtoTOEntity(historiaRequest);
+            modelHistoria = historiaService.add(modelHistoria);
+            HistoriaRequest creaHistoriaRequest = historiaMapper.entityToDto(modelHistoria);
+            return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponseH<>(HttpStatus.CREATED.value(), 
+            "Historia creado", creaHistoriaRequest));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+            "Error al crear Historia", null));
+        }
+    }
+
+    //Modificar paciente
+    @PutMapping("/update")
+    public ResponseEntity<ApiResponseH<HistoriaRequest>> update(@RequestBody HistoriaRequest historiaRequest) {
+        try{
+            modelHistoria historia = historiaService.update(historiaMapper.dtoTOEntity(historiaRequest));
+            HistoriaRequest actualizarHistoriaRequest = historiaMapper.entityToDto(historia);
+            return ResponseEntity.ok(new ApiResponseH<>(HttpStatus.OK.value(),
+             "Paciente actualizado", actualizarHistoriaRequest));
+        }catch(HistoriaNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ApiResponseH<>(HttpStatus.NOT_FOUND.value(), 
+            ex.getMessage(), null));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "Error al actualizar paciente", null));
+        }
+    }
+
+}
