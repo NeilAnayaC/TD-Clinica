c43b3acd434b73db16f8d5da2086bd1e39cff5f9
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-18 23:44:05 -0500

    Create service detalle Postgre

--- MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/service/DetalleService.java
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/service/DetalleService.java
@@ -1,5 +1,45 @@
 package Clinica.MSHisctoriaC.service;
 
-public class DetalleService {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Clinica.MSHisctoriaC.Exection.HistoriaNotFoundException;
+import Clinica.MSHisctoriaC.model.modelDetalle;
+import Clinica.MSHisctoriaC.repository.IDetalleRepository;
+
+@Service
+public class DetalleService implements IDetalleService{
     
+
+    @Autowired
+    IDetalleRepository detalleRepository;
+
+    @Override 
+    public modelDetalle add (modelDetalle detalle){
+        return detalleRepository.save(detalle);
+    }
+
+    @Override
+    public List<modelDetalle> findAll(){
+        return (List<modelDetalle>) detalleRepository.findAll();
+    }
+
+    @Override
+    public modelDetalle findById(Integer id){
+        Optional<modelDetalle> detalles = detalleRepository.findById(id);
+        return detalles.get();
+    }
+
+    @Override
+    public modelDetalle update(modelDetalle detalle){
+        if (detalleRepository.existsById(detalle.getIddetalle())) {
+            return detalleRepository.save(detalle);
+        }else{
+            throw new HistoriaNotFoundException("No se puede actualizar. Historia no encontrado con ID:"+ detalle.getIddetalle());
+        }
+    }
+
 }
--- MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/service/IDetalleService.java
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/service/IDetalleService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import Clinica.MSHisctoriaC.model.modelDetalle;
 
 public interface IDetalleService {
-    public modelDetalle add(modelDetalle detalle);
+    public modelDetalle add (modelDetalle detalle);
     public List<modelDetalle> findAll();
     public modelDetalle findById(Integer id);
     public modelDetalle update(modelDetalle historia);
