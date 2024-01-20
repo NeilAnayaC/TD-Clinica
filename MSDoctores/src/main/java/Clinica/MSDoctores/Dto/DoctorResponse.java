c55305470e0785d93b6dbe02574eb2be88c23d0c
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-19 23:07:38 -0500

    Create Dto

---
+++ MSDoctores/src/main/java/Clinica/MSDoctores/Dto/DoctorResponse.java
@@ -0,0 +1,49 @@
+package Clinica.MSDoctores.Dto;
+
+public class DoctorResponse {
+    private String id;
+    private String nombred;
+    private String apellidod;
+    private String especialidadd;
+    private String celular;
+    private String correo;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombred() {
+        return nombred;
+    }
+    public void setNombred(String nombred) {
+        this.nombred = nombred;
+    }
+    public String getApellidod() {
+        return apellidod;
+    }
+    public void setApellidod(String apellidod) {
+        this.apellidod = apellidod;
+    }
+    public String getEspecialidadd() {
+        return especialidadd;
+    }
+    public void setEspecialidadd(String especialidadd) {
+        this.especialidadd = especialidadd;
+    }
+    public String getCelular() {
+        return celular;
+    }
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    
+}
