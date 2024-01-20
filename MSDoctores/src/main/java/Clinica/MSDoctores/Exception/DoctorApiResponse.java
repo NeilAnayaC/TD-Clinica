f6597feff6cda48fa1ede15a5fc6643bd27c3dc8
RenzoTorresA <133610404+RenzoTorresA@users.noreply.github.com>
2024-01-19 23:07:28 -0500

    Create exception

---
+++ MSDoctores/src/main/java/Clinica/MSDoctores/Exception/DoctorApiResponse.java
@@ -0,0 +1,32 @@
+package Clinica.MSDoctores.Exception;
+
+public class DoctorApiResponse<T> {
+    
+    private final int status;
+    private final String mensaje;
+    private final T data;
+
+    
+    public DoctorApiResponse(int status, String mensaje, T data) {
+        this.status = status;
+        this.mensaje = mensaje;
+        this.data = data;
+    }
+
+
+    public int getStatus() {
+        return status;
+    }
+
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+
+    public T getData() {
+        return data;
+    }
+
+    
+}
---
+++ MSDoctores/src/main/java/Clinica/MSDoctores/Exception/DoctorNotFoundException.java
@@ -0,0 +1,7 @@
+package Clinica.MSDoctores.Exception;
+
+public class DoctorNotFoundException extends RuntimeException{
+    public DoctorNotFoundException(String message){
+        super(message);
+    }
+}
