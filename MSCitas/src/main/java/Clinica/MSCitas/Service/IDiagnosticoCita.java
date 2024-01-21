70f7e4e9a2d4f0e829e1a434b549ecc2d0d3b215
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-21 14:00:09 -0500

    Update IDiagnosticoCita.java

--- MSCitas/src/main/java/Clinica/MSCitas/Service/IDiagnosticoCita.java
+++ MSCitas/src/main/java/Clinica/MSCitas/Service/IDiagnosticoCita.java
@@ -1,5 +1,14 @@
 package Clinica.MSCitas.Service;
 
+import java.util.List;
+
+//import Clinica.MSCitas.Model.ModelCitas;
+import Clinica.MSCitas.Model.ModelDiagnosticoCita;
+
 public interface IDiagnosticoCita {
-    
+    public ModelDiagnosticoCita crearDiagnosticocita(ModelDiagnosticoCita diagnostico);
+    public List<ModelDiagnosticoCita> listarDiagnosticocita();
+    public ModelDiagnosticoCita buscarIdDiagnosticocita(int id);
+    public ModelDiagnosticoCita actualizarDiagnosticocita(ModelDiagnosticoCita diagnostico);
+    public boolean deleteDiagnosticocita(int id);
 }
