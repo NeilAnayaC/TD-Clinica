9462691e41507b0ab0599907c291af9a1d51a8f5
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-21 14:00:02 -0500

    Update DiagnosticoCitaService.java

--- MSCitas/src/main/java/Clinica/MSCitas/Service/DiagnosticoCitaService.java
+++ MSCitas/src/main/java/Clinica/MSCitas/Service/DiagnosticoCitaService.java
@@ -1,5 +1,51 @@
 package Clinica.MSCitas.Service;
 
-public class DiagnosticoCitaService {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Clinica.MSCitas.Exception.CitasNotFoundException;
+//import Clinica.MSCitas.Model.ModelCitas;
+import Clinica.MSCitas.Model.ModelDiagnosticoCita;
+//import Clinica.MSCitas.Repository.ICitasRepository;
+import Clinica.MSCitas.Repository.IDiagnosticoCitaRepository;
+
+@Service
+public class DiagnosticoCitaService implements IDiagnosticoCita{
     
+    @Autowired
+    IDiagnosticoCitaRepository diagnosticoCitaRepository;
+
+    @Override
+    public ModelDiagnosticoCita crearDiagnosticocita (ModelDiagnosticoCita diagnostico){
+        return diagnosticoCitaRepository.save(diagnostico);
+    }
+
+    @Override
+    public List<ModelDiagnosticoCita> listarDiagnosticocita(){
+        return (List<ModelDiagnosticoCita>) diagnosticoCitaRepository.findAll();
+    }
+
+    @Override 
+    public ModelDiagnosticoCita buscarIdDiagnosticocita(int id){
+        Optional<ModelDiagnosticoCita> diagnostico = diagnosticoCitaRepository.findById(id);
+        return diagnostico.get();
+    }
+
+    @Override 
+    public ModelDiagnosticoCita actualizarDiagnosticocita(ModelDiagnosticoCita diagnostico){
+        if (diagnosticoCitaRepository.existsById(diagnostico.getDiagnosticoid())) {
+            return diagnosticoCitaRepository.save(diagnostico);
+        }else {
+            throw new CitasNotFoundException("no se puede actualizar Citas");
+        }
+    }
+
+    @Override
+    public boolean deleteDiagnosticocita(int id){
+        diagnosticoCitaRepository.deleteById(id);
+        return true;
+    }
 }
