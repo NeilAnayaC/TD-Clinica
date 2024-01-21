75b742c80f394b775548b4cbc187c06eb60b3094
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-21 14:00:06 -0500

    Update ICitasService.java

--- MSCitas/src/main/java/Clinica/MSCitas/Service/ICitasService.java
+++ MSCitas/src/main/java/Clinica/MSCitas/Service/ICitasService.java
@@ -1,5 +1,14 @@
 package Clinica.MSCitas.Service;
 
+import java.util.List;
+
+import Clinica.MSCitas.Model.ModelCitas;
+
 public interface ICitasService {
     
+    public ModelCitas crearcita(ModelCitas citas);
+    public List<ModelCitas> listarCita();
+    public ModelCitas buscarIdCita(int id);
+    public ModelCitas actualizarCita(ModelCitas citas);
+    public boolean deleteCita(int id);
 }
