b4a5f68b65a43e85353628da07c42acb284304e5
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-21 23:37:11 -0500

    Update HistoriaController.java

--- MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/Controller/HistoriaController.java
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/Controller/HistoriaController.java
@@ -101,7 +101,7 @@ public class HistoriaController {
         }catch(Exception ex){
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
             .body(new ApiResponseH<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            "Error al actualizar paciente", null));
+            "Error al actualizar historia", null));
         }
     }
 
