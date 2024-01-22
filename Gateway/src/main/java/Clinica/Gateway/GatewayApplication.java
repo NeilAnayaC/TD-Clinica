081312637faf79499c88416789708833d24bc4ee
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-21 23:37:56 -0500

    Update GatewayApplication.java

--- Gateway/src/main/java/Clinica/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/Clinica/Gateway/GatewayApplication.java
@@ -2,9 +2,8 @@ package Clinica.Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
