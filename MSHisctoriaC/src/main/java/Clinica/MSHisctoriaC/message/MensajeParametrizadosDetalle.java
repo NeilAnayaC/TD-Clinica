7ab66da1c880063454ef7d0606a32515953da311
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-23 14:54:15 -0500

    Create MensajeParametrizadosDetalle.java

---
+++ MSHisctoriaC/src/main/java/Clinica/MSHisctoriaC/message/MensajeParametrizadosDetalle.java
@@ -0,0 +1,19 @@
+package Clinica.MSHisctoriaC.message;
+
+public class MensajeParametrizadosDetalle {
+    
+
+    public static final String MENSAJE_CREAR_DETALLE_EXITOSO = "MSDETALLE PACIENTE creado exitosamente.";
+    public static final String MENSAJE_CREAR_DETALLE_NOEXITOSO = "MSDETALLE PACIENTE NO CREADO.";
+    public static final String MENSAJE_DETALLE_EXISTENTE = "MSDETALLE - El PACIENTE ya existe";
+    public static final String MENSAJE_DETALLE_LISTADO = "MSDETALLE - LISTADO EXITOSAMENTE";
+    public static final String MENSAJE_DETALLE_LISTADOID = "MSDETALLE - BUSQUEDA ID EXITOSAMENTE";
+    public static final String MENSAJE_DETALLE_NO_ENCONTRADO = "MSDETALLE - PACIENTE no encontrado:";
+    public static final String MENSAJE_DETALLE_NO_ENCONTRADOELIMINAR = "MSDETALLE - PACIENTE no encontrado a eliminar:";
+    public static final String MENSAJE_DETALLE_NO_ENCONTRADO_ID = "MSDETALLE -PACIENTE no encontrado: ";
+    public static final String MENSAJE_DETALLE_EDITADO_EXITOSO = "MSDETALLE - PACIENTE editado exitosamente";
+    public static final String MENSAJE_ERROR_BASE_DATOS_DETALLE = "MSDETALLE - Error en la base de datos: ";
+    public static final String MENSAJE_ERROR_INTERNO_SERVIDOR_DETALLE = "MSDETALLE - Error interno del servidor: ";
+    public static final String MENSAJE_ELIMINAR_DETALLE_EXITOSO = "MSDETALLE - PACIENTE eliminado exitosamente";
+    public static final String MENSAJE_ERROR_DETALLE = "MSDETALLE - Error ";
+}
