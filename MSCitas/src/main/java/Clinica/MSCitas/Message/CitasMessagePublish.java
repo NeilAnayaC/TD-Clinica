67e54affa0a889deeaee38062ba2819aff89c052
RenzoTorresA <brayantorresacevedo@gmail.com>
2024-01-22 22:11:11 -0500

    Update CitasMessagePublish.java

--- MSCitas/src/main/java/Clinica/MSCitas/Message/CitasMessagePublish.java
+++ MSCitas/src/main/java/Clinica/MSCitas/Message/CitasMessagePublish.java
@@ -19,7 +19,7 @@ public class CitasMessagePublish {
     // private Logger log = LoggerFactory.getLogger(PayMessagePublish.class);
 
     @Autowired
-    KafkaTemplate<String, String> kafkaTemplate;
+    KafkaTemplate<Integer, String> kafkaTemplate;
 
     @Autowired
     ObjectMapper objectMapper;
